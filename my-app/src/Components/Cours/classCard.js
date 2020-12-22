a3ceb667532d0d039f5a56a77fd0afd74eaab637
romssprogrammer <romain.baste@gmail.com>
2020-12-22 13:47:49 +0100

    apres midi

--- my-app/src/Components/Cours/classCard.js
+++ my-app/src/Components/Cours/classCard.js
@@ -6,17 +6,19 @@ import styles from "./classCard.module.css"
 
 const ClassCard= (props)=>{
     //Destructuration de props
+    //const transfimage = "transform scale-75"
+    //styles.classCard
     
-    const {titre,enseignement,image,nomProfesseur,couleur} = props
+    const {titre,enseignement,image,nomProfesseur,couleur,imgSize} = props
     return(
         
          
         <div >
            <div className=   {couleur}>
             <h1 className= "text-center ">{titre}</h1>
-            
-            
-            <img className= {styles.classCard}  src={image} alt=""/>
+
+           
+            <img className src={image} alt=""/>
             <div class="font-bold text-xl mb-2"> 
             <p>{enseignement}</p>
             </div>
--- my-app/src/Components/Cours/classCardContainer.jsx
+++ my-app/src/Components/Cours/classCardContainer.jsx
@@ -1,15 +1,16 @@
 import ClassCard from "./classCard"
 import text from "../../Data"
+
 const ClassCardContainer = ()=>{
+    // const imageSize= ClassCard.attribut ??
    
     return(
         
-        <div className= "mx-auto flex items-center">
-            <div className = "mr-8 "> <ClassCard  titre="dessin" enseignement= "blablalblblablllalblablab" image ="elfe_lady.png" couleur="bg-blue-400 rounded-lg"/> </div>
-            <div className ="mr-8"><ClassCard titre="Sculpture" enseignement="blablalblblablllalblablab" image ="sisters-claire-wendling-signe.jpg" couleur ="bg-yellow-500 rounded-lg" /></div>
-            
-            <div className ="mr-8"><ClassCard titre="Peinture" enseignement={text} image ="elfe_lady.png" couleur = "bg-indigo-500"/></div>
-            <div className ="mr-8" ><ClassCard titre="Gravure" enseignement="blablalblblablllalblablab" image ="elfe_lady.png"/></div>
+        <div className= "mx-50 flex items-center">
+            <div className ="mr-8 "> <ClassCard  titre="dessin" enseignement= {text} image ="elfe_lady.png" couleur="bg-blue-400 rounded-lg"/> </div>
+            <div className ="mr-8"><ClassCard titre="Sculpture" enseignement={text} image ="sisters-claire-wendling-signe.jpg" couleur ="bg-yellow-500 rounded-lg" /></div>
+            <div className ="mr-8"><ClassCard titre="Peinture" enseignement={text} image ="elfe_lady.png" couleur = "bg-indigo-500 rounded-lg" /></div>
+            <div className ="mr-8" ><ClassCard titre="Gravure" enseignement={text} image ="elfe_lady.png" couleur = "bg-gray-400 rounded-lg"/></div>
            
            
         </div>
